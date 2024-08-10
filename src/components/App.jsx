1b6a8745232e0a17192e7916b0d97c5b706530e0
AndreyTara <explorituse@gmail.com>
2024-08-10 15:49:43 +0300

    'adds-sty-local'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,17 @@ import Counter from "./Counter/Counter";
 import ContactList from "./ContactList/ContactList";
 import SearchBox from "./SearchBox/SearchBox";
 import ContactForm from "./ContactForm/ContactForm";
-
+import css from "./App.module.css";
 // import "./index.css";
 
 const App = () => {
   return (
-    <>
-      <Counter />
+    <div className={css.wrap}>
+      {/* <Counter /> */}
+      <SearchBox />
       <ContactForm />
       <ContactList />
-      <SearchBox />
-    </>
+    </div>
   );
 };
 
